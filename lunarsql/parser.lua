f06713d9f77330315f046696528f3a3778615fb5
Perry <aidanpj18@gmail.com>
2025-02-13 21:56:24 -0500

    wite Parser

--- lunarsql/parser.lua
+++ lunarsql/parser.lua
@@ -75,3 +75,90 @@ function Lexer:tokenize(input)
     end
     return tokens
 end
+
+-- Parser struct
+local Parser = {}
+function Parser.new()
+    local self = setmetatable({}, Parser)
+    self.current = 1
+    return self
+end
+
+function Parser:parse(tokens)
+    self.tokens = tokens
+    self.current = 1
+    return self:parse_select()
+end
+
+function Parser:match(token_type)
+    if self.current <= #self.tokens and self.tokens[self.current].type == token_type then
+        self.current = self.current + 1
+
+        return true
+    end
+    return false
+end
+
+function Parser:parse_select()
+    if not self:match("SELECT") then
+        error("Expected SELECT")
+    end
+
+    local columns = self:parse_column_list()
+
+    if not self:match("FROM") then
+        error("Expected FROM")
+    end
+
+    if self.current > #self.tokens then
+        error("Expected table name")
+    end
+
+    local table_name = self.tokens[self.current].value
+    self.current = self.current + 1
+
+    local where_clause = nil
+
+    if self.current <= #self.tokens and self.tokens[self.current].type == "WHERE" then
+        self.current = self.current + 1
+
+        where_clause = self:parse_where_clause()
+    end
+
+    return {
+        type = "SELECT",
+        columns = columns,
+        table = table_name,
+        where = where_clause
+    }
+end
+
+function Parser:parse_column_list()
+    local columns = {}
+
+    while true do
+        if self.current > #self.tokens then
+            error("Unexpected end of input")
+        end
+
+        if self.tokens[self.current].type == "IDENTIFIER" then
+            table.insert(columns, self.tokens[self.current].value)
+            self.current = self.current + 1
+        end
+
+        if self.current > #self.tokens or self.tokens[self.current].type ~= "COMMA" then
+            break
+        end
+        self.current = self.current + 1
+    end
+    return columns
+end
+
+function Parser:parse_where_clause()
+    local clause = {}
+    while self.current <= #self.tokens do
+        table.insert(clause, self.tokens[self.current].value)
+        self.current = self.current + 1
+    end
+    return table.concat(clause, " ")
+end
